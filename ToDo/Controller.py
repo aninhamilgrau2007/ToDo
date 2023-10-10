406d21ea7ce3dbc7b601b98171df0dd1cdb1ca77
aninhamilgrau2007 <140507231+aninhamilgrau2007@users.noreply.github.com>
2023-10-10 15:47:07 -0300

    Add files via upload

---
+++ ToDo/Controller.py
@@ -0,0 +1,47 @@
+from Model import *
+import os
+
+class ControllerAdicionarTarefa():
+    def __init__(self, tarefa):
+
+        try:
+            if tarefa == "":
+                print("Digite uma tarefa válida.")
+
+            else:
+
+                try:
+                    self.tarefa = tarefa
+                    if TODO.AdicionarTarefa(self.tarefa) == True:
+                        print("Tarefa adicionada.")
+                    else:
+                        print("Algum problema foi encontrado.")
+
+                except Exception as erro:
+                    print("Erro ao adicionar a tarefa: {erro}")
+
+        except Exception as erro:
+                print("Erro ao adicionar a tarefa: {erro}")
+
+class ControllerExcluirTarefa():
+    def __init__(self, excluir):
+        self.excluir = excluir
+
+        try:
+            if TODO.RemoverTarefa(self.excluir) == True and self.excluir >= "0":
+                print("Tarefa removida.")
+                    
+            else:
+                print("Algum problema foi encontrado.")
+
+        except Exception as erro:
+                print("Digite um número válido. Esta tarefa não existe.")
+
+class ControllerListarTarefa():
+    def __init__(self):
+        ControllerLista = TODO.ListarTarefas()
+        cont = 0
+
+        for tarefas in ControllerLista:
+            cont += 1
+            print(f"{cont}. {tarefas}")
---
+++ ToDo/View.py
@@ -0,0 +1,43 @@
+from Controller import *
+from dao import *
+import os
+
+sair = 0
+while sair == 0:
+
+    os.system("cls")
+    print("SOFTWARE DE TO-DO \n1 - Adicionar tarefa \n2 - Listar tarefas \n3 - Remover tarefa \n4 - Sair")
+    opcao = input("Digite a opção desejada > ")
+
+    match opcao:
+        case "1":
+            os.system("cls")
+            tarefa = input("Digite a tarefa > ")
+            adicionarTarefa = ControllerAdicionarTarefa(tarefa)
+            adicionarTarefa = DaoAdicionarTarefa(tarefa)
+            os.system("pause")
+
+    
+        case "2":
+            os.system("cls")
+            print("Estes são os itens presentes na lista de tarefas: ")
+            listarTarefa = ControllerListarTarefa()
+            print("Estes são os itens presentes no DAO: ")
+            listarTarefa = DaoListarTarefa()
+            os.system("pause")
+
+        case "3":
+            os.system("cls")
+            listarTarefa = ControllerListarTarefa()
+            excluir = input("Digite o número da tarefa que deseja excluir > ")
+            excluirTarefa = ControllerExcluirTarefa(excluir)
+            excluirTarefa = DaoExcluirTarefa(excluir)
+            os.system("pause")
+
+        case "4":
+            os.system("cls")
+            sair = 1
+
+        case _:
+            os.system("cls")
+            print("Opção inválida")
